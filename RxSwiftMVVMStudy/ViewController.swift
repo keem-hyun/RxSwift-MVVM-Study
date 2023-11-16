54c463fea6935c9c86024258d6a364d21e864659
keem-hyun <hw813@naver.com>
2023-11-16 21:48:33 +0900

    add Compositional Layout + DiffableDatasource

--- RxSwiftMVVMStudy/ViewController.swift
+++ RxSwiftMVVMStudy/ViewController.swift
@@ -10,19 +10,40 @@ import UIKit
 class ViewController: UIViewController {
     
     private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.register(BannerCollectionViewCell.self, forCellWithReuseIdentifier: BannerCollectionViewCell.id)
+        collectionView.setCollectionViewLayout(createLayout(), animated: true)
+        setDataSource()
     }
     
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout(sectionProvider: {[weak self] sectionIndex, _ in
+            
+            return self?.createBannerSection()
+        })
+    }
     
+    private func createBannerSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        return section
+    }
+    
+    private func setDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BannerCollectionViewCell.id, for: indexPath) as? BannerCollectionViewCell else { return UICollectionViewCell() }
+//            cell.config(title: <#T##String#>, imageUrl: <#T##String#>)
+            return cell
+        })
+    }
 }
-
-
-
-// 컬렉션뷰 cell UI - 등록
-// 레이아웃 구현 - 3가지
-// datasource -> cellProvider
-// snpashot -> datasource.apply(snapshot)
